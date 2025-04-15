ac268e17ffe6cf6da85f50286358f9429161780a
Karthik sundaram G <akashkarthi10g@gmail.com>
2025-04-15 12:11:54 +0530

    Update LoginSignup.jsx

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -17,7 +17,7 @@ const LoginSignup = () => {
   const login = async () => {
     console.log("Login",formData)
     let responseData;
-    await fetch('http://localhost:4000/login',{
+    await fetch('https://e-commerce-backend-chhq.onrender.com/login',{
       method:'POST',
       headers:{
         Accept:'application/json',
@@ -39,7 +39,7 @@ const LoginSignup = () => {
   const signup = async () => {
     console.log("Signup",formData)
     let responseData;
-    await fetch('http://localhost:4000/signup',{
+    await fetch('https://e-commerce-backend-chhq.onrender.com/signup',{
       method:'POST',
       headers:{
         Accept:'application/json',
